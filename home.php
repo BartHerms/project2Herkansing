8867c0b7eb3167b69415d4581d0ea268beeb3d5e
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2022-11-29 14:35:22 +0100

    Static data with visual lines

--- home.php
+++ home.php
@@ -9,5 +9,40 @@
 </head>
 <body>
     <?php include_once("menu/header.html"); ?>
+    <h1> Goedemorgen (Naam uit db)</h1>
+
+    <div class="fastview">
+        <div class="row">
+        <h3>Tickets</h3>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+        </div>    
+        <div class="row">
+            <h3>Diensten</h3>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+            <div class="entry">
+                <p> onderwerp ticket </p>
+            </div>
+        </div>    
+    </div>
+
 </body>
 </html>
