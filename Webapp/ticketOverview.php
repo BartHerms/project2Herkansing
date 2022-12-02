3ecd68a4c2e6d4d1e10e7264297d2ac372dcc85c
RikSchippers <rik.schippers@student.nhlstenden.com>
2022-12-02 14:21:32 +0100

    basics af

---
+++ Webapp/ticketOverview.php
@@ -0,0 +1,25 @@
+﻿﻿<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            include 'classes/Ticket.php';
+
+            define("SERVER_IP", "localhost"); 
+            
+            $db = mysqli_connect(SERVER_IP, "root", null, "project2");
+            $result = $db->query("CALL getTickets()");
+            $db->close();
+            $rowCount = $result->num_rows;
+
+            for ($counter = 1; $counter <= $rowCount; $counter++){
+                $Ticket = new Ticket();
+                $Ticket->setTicket($result);
+                echo "<div>'{$Ticket->getOnderwerp()}'</div>";
+            }
+        ?>
+    </body>
+</html>
