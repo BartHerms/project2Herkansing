62992647ff5ea596c1813f3f0dfdf764be75c889
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2022-12-07 15:24:22 +0100

    remove unnessecary files

--- getMedewerker.sql
+++
@@ -1,8 +0,0 @@
-
-DELIMITER &&  
-CREATE PROCEDURE getMedewerker (IN emailadressMedewerker char(255))
-BEGIN
-SELECT emailadress, voornaam, achternaam, administrator
-FROM medewerker
-WHERE emailadress = emailadressMedewerker;
-END &&
