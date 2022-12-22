cf193d40938df4f0e79f62e21e7005f489c16dbc
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-12-22 10:25:15 +0100

    sessions
    sessions

--- mainPageTest.php
+++ mainPageTest.php
@@ -1,5 +1,32 @@
 <?php session_start (); 
-if(!isset($_SESSION["userEmail"])) ?>
+if(!isset($_SESSION["userEmail"])) 
+
+var_dump($_SESSION["userEmail"]);
+
+
+		
+		if(isset($_GET['page'])){
+			
+			include($_GET['page']. ".php");
+		}
+		else{
+			
+			if(isset($_SESSION['loggedIn'])){
+				
+				echo "<p>You are logged in</p>";
+				echo "<a href='index.php?page=logout'>click here to log out</a>";
+				echo $_SESSION['image'];
+			}
+			else{
+				
+				echo "session not set<br /><br />";
+				include("login.php");
+			}
+			
+		} 
+	
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
