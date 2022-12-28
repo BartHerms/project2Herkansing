8802acb7c0bf8bd4bf1aa27831c530f78457a376
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-12-28 14:47:05 +0100

    beetje stylen
    beetje stylen

--- loginPagina.php
+++ loginPagina.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" type="text/css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+<div>
     <form action="loginhelper.php" method="post">
     <input type="text" name="email" required>
     <input type="password" name="password" required>
     <input type="submit" name="submitlogin">
     </form>
+</div>
 </body>
 </html>
