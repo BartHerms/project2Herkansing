740b0096721a4dfc17fde7907459bc210d6e0d23
BartHerms <91067528+BartHerms@users.noreply.github.com>
2023-01-09 10:16:48 +0100

    fixes voor login pagina
    fixes voor login pagina

--- loginhelper.php
+++ loginhelper.php
@@ -1,19 +1,5 @@
 <?php session_start(); ?>
-<?php include 'LoginKlant.php' ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-
+<?php include 'Klant.php' ?>
 
 <?php
 if(isset($_POST['submitlogin']))
@@ -21,17 +7,16 @@ if(isset($_POST['submitlogin']))
     $userEmail = $_POST['email'];
     $userPassword = $_POST['password'];
 
-    $_SESSION['email'] = $userEmail;
-    $_SESSION['loggedIn'] = true;
-    
     $db = mysqli_connect("localhost", "root", "", "project2");
     $result = $db->query("call getLoginValue('{$userEmail}')");
     $db-> close();
-    $LoginKlant = new LoginKlant();
-    $LoginKlant-> setLoginKlant($result);
-    if ($LoginKlant-> getPassword() == $userPassword)
-    {
-        header("location:mainPageTest.php");
+    $LoginKlant = new Klant();
+    $LoginKlant-> setKlant($result);
+    if (password_verify( $LoginKlant-> getPassword(), $userPassword))
+    {   
+        $_SESSION['email'] = $userEmail;
+        $_SESSION['loggedIn'] = true;
+        header("location:home.php");
     }
     else
     {
