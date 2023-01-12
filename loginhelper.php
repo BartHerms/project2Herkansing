1207e22f8b51503af281acf01602183e0c56f1ed
BartHerms <91067528+BartHerms@users.noreply.github.com>
2023-01-12 11:44:13 +0100

    update login voor medewerker
    zou nu moeten kunnen zien of het een medewerker is

--- loginhelper.php
+++ loginhelper.php
@@ -1,22 +1,33 @@
 <?php session_start(); ?>
-<?php include 'Klant.php' ?>
+<?php include 'loginKlant.php' ?>
 
 <?php
 if(isset($_POST['submitlogin']))
 {
     $userEmail = $_POST['email'];
     $userPassword = $_POST['password'];
+    $isMedewerker = strpos($userEmail, "serviceit.nl");
 
     $db = mysqli_connect("localhost", "root", "", "project2");
     $result = $db->query("call getLoginValue('{$userEmail}')");
+    // call moet nog aangepast worden
     $db-> close();
-    $LoginKlant = new Klant();
-    $LoginKlant-> setKlant($result);
+    $LoginKlant = new loginKlant();
+    $LoginKlant-> setLoginKlant($result);
     if (password_verify( $LoginKlant-> getPassword(), $userPassword))
+    // password verify bestaat nog niet in de loginklant class
     {   
         $_SESSION['email'] = $userEmail;
         $_SESSION['loggedIn'] = true;
         
+        if ($isMedewerker == true)
+            {
+                $_SESSION['isKlant'] = false;
+            }
+        else
+            {
+                $_SESSION['isKlant'] = true;
+            }
         //if useremail contains service it
         //then $_session isklant = false
         //otherwise = true
