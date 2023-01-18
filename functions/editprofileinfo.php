98c1c0830a5c5fda842dd4c66322c36d4149ac7f
Leroy Kats <leroy.kats@student.nhlstenden.com>
2023-01-18 14:12:34 +0100

    input checks added

--- functions/editprofileinfo.php
+++ functions/editprofileinfo.php
@@ -1,22 +1,26 @@
 <?php
 if(isset($_POST["submit"])){
-    $sql = "UPDATE klant SET adres = ?, postcode = ?, telefoonnummer = ?, emailadress = ? WHERE emailadress = ?";
+    if(!empty($_POST["adres"]) AND !empty($_POST["postcode"]) AND !empty($_POST["telephone"]) AND !empty($_POST["email"])){
+        $sql = "UPDATE klant SET adres = ?, postcode = ?, telefoonnummer = ?, emailadress = ? WHERE emailadress = ?";
 
-    $adress = filter_input(INPUT_POST, "adres", FILTER_SANITIZE_STRING);
-    $postcode = filter_input(INPUT_POST, "postcode", FILTER_SANITIZE_STRING);
-    $telephonenumber = filter_input(INPUT_POST, "telephone", FILTER_SANITIZE_NUMBER_INT);
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $adress = filter_input(INPUT_POST, "adres", FILTER_SANITIZE_STRING);
+        $postcode = filter_input(INPUT_POST, "postcode", FILTER_SANITIZE_STRING);
+        $telephonenumber = filter_input(INPUT_POST, "telephone", FILTER_SANITIZE_NUMBER_INT);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
 
 
-    if($statement = mysqli_prepare($connection, $sql)) {
-        mysqli_stmt_bind_param($statement, 'ssiss', $adress, $postcode, $telephonenumber, $email, $email);
+        if($statement = mysqli_prepare($connection, $sql)) {
+            mysqli_stmt_bind_param($statement, 'ssiss', $adress, $postcode, $telephonenumber, $email, $email);
 
-        if(mysqli_stmt_execute($statement)) {
-            echo "Query succesvol uitgevoerd!";
-        } else {
-            echo "Error tijdens uitvoeren van query";
-            die (mysqli_error($connection));
+            if(mysqli_stmt_execute($statement)) {
+                echo "Query succesvol uitgevoerd!";
+            } else {
+                echo "Error tijdens uitvoeren van query";
+                die (mysqli_error($connection));
+            }
         }
+    }else{
+        echo "Niet alle velden zijn ingevuld!";
     }
 }
 ?>
