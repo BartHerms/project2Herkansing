07237b965159cdc27913111d5027ff9b83e355b0
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2023-01-20 09:30:57 +0100

    use correct login script

--- loginPagina.php
+++ loginPagina.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <div>
-    <form id="form" action="loginhelper.php" method="post">
+    <form id="form" action="loginh.php" method="post">
     <input type="text" id="text" name="email" placeholder="E-mail" required>
     <input type="password" id="password" name="password" placeholder="Wachtwoord" required>
     <input type="submit" id="submitbutton" name="submitlogin">
