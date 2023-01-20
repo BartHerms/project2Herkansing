d2b91312d03b8e7a37a044322b034651251dfd25
RikSchippers <rik.schippers@student.nhlstenden.com>
2023-01-20 12:11:01 +0100

    css toegevoegd

--- pages/serviceRequestFormProcess.php
+++ pages/serviceRequestFormProcess.php
@@ -13,7 +13,10 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link href="../style.css" type="text/css" rel="stylesheet">
+       <title>Service IT</title>
     </head>
     <body>
         <?php
